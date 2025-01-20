f87551896c82dd4070d0364edcfe09f7d8aea7fc
Axel Meyrek Mac <chavezsud1406@gmail.com>
2025-01-20 10:31:17 -0600

    Restructuracion

--- index.js
+++ index.js
@@ -8,42 +8,49 @@ const $charlas = document.querySelector('#charlas');
 
 
 /* FUNCIONES */
-const addEventButtons = () => {
-    $buttonDias.forEach(button => button.addEventListener('click', renderCharlas));
-}
+const addEventButtons = () => $buttonDias.forEach(button => button.addEventListener('click', renderCharlas));
+
+const desactiveAllButtons = () => $buttonDias.forEach(button => button.classList.remove('buttonActive'));
 
 const consumirDatos = async () => {
     const response = await fetch('charlas.json');
     const data = await response.json();
     charlas = data.charlas;
+    renderCharlas();
 }
 
 const renderCharlas = (e) => {
-    desactiveAllButtons();
-    e.target.classList.add('buttonActive');
-    const diaSeleccionado = e.target.textContent;
+    let diaSeleccionado;
+
+    if(e == undefined){
+        diaSeleccionado = 'Lunes';
+    }else{
+        diaSeleccionado = e.target.textContent;
+        desactiveAllButtons();
+    }
 
     const charlasSeleccionadas = charlas.filter(charla => charla.dia === diaSeleccionado);
 
     $charlas.innerHTML = '';
+    
     charlasSeleccionadas.forEach(charla => {
+        /* DESECTRUCTURACIÓN */
+        const {hora, titulo, ponente, subtitulo, descripcion, ubicacion} = charla;
         const $charla = /* html */
             ` <article class="charlas">
-                    <p class="charlas_hora">${charla.hora}</p>
-                    <h3 class="charlas_title">${charla.titulo}</h3>
-                    <h4 class="charlas_ponente">${charla.ponente}</h4>
-                    <p class="charlas_subtitle">${charla.subtitulo}</p>
-                    <p class="charlas_description">${charla.descripcion}</p>
-                    <p class="charlas_ubicacion">📍 ${charla.ubicacion}</p>
+                    <p class="charlas_hora">${hora}</p>
+                    <h3 class="charlas_title">${titulo}</h3>
+                    <h4 class="charlas_ponente">${ponente}</h4>
+                    <p class="charlas_subtitle">${subtitulo}</p>
+                    <p class="charlas_description">${descripcion}</p>
+                    <p class="charlas_ubicacion">📍 ${ubicacion}</p>
                 </article>`;
 
         $charlas.innerHTML += $charla;
     });
 }
 
-const desactiveAllButtons = () => {
-    $buttonDias.forEach(button => button.classList.remove('buttonActive'));
-}
+
 
 
 
