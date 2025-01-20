c8fe0318c77cf456b1868110029f8061726770a2
Axel Meyrek Mac <chavezsud1406@gmail.com>
2025-01-20 01:55:52 -0600

    Add file json

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 /* VARIABLES Y COMPONENTES */
+let charlas = [];
 
+const $buttonDias = document.querySelectorAll('.programa_button');
+
+const $charlas = document.querySelector('#charlas');
 /* FUNCIONES */
+const addEventButtons = () => {
+    $buttonDias.forEach(button => button.addEventListener('click', renderCharlas));
+}
 
+const consumirDatos = async () => {
+    const response = await fetch('charlas.json');
+    const data = await response.json();
+    charlas = data.charlas;
+}
+
+const renderCharlas = (e) => {
+    const diaSeleccionado = e.target.textContent;
+    
+    const charlasSeleccionadas = charlas.filter(charla => charla.dia === diaSeleccionado);
+    
+    $charlas.innerHTML = '';
+    charlasSeleccionadas.forEach(charla => {
+        const $charla = /* html */
+                ` <article class="charlas">
+                    <p class="charlas_hora">${charla.hora}</p>
+                    <h3 class="charlas_title">${charla.titulo}</h3>
+                    <h4 class="charlas_ponente">${charla.ponente}</h4>
+                    <p class="charlas_subtitle">Ceremonia de apertura del 5to Encuentro de Ingenierías UPAEP</p>
+                    <p class="charlas_description">${charla.descripcion}</p>
+                    <p class="charlas_ubicacion">📍 ${charla.ubicacion}</p>
+                </article>`;
+
+        $charlas.innerHTML += $charla;
+})
+
+
+}
 /* EVENTOS */
+document.addEventListener('DOMContentLoaded', addEventButtons);
+
+document.addEventListener('DOMContentLoaded', consumirDatos);
