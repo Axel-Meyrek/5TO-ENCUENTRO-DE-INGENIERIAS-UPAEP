f28d17625bdaaa5ae4c19355aa22d12ae0666761
Axel Meyrek Mac <chavezsud1406@gmail.com>
2025-01-20 20:43:23 -0600

    Update of the Files

--- js/index.js
+++ js/index.js
@@ -5,6 +5,14 @@ const $buttonDias = document.querySelectorAll('.programa_button');
 
 const $charlas = document.querySelector('#charlas');
 
+const $dias = document.querySelector('#dias');
+
+const $horas = document.querySelector('#horas');
+
+const $minutos = document.querySelector('#minutos');
+
+const $segundos = document.querySelector('#segundos');
+
 
 
 /* FUNCIONES */
@@ -13,7 +21,7 @@ const addEventButtons = () => $buttonDias.forEach(button => button.addEventListe
 const desactiveAllButtons = () => $buttonDias.forEach(button => button.classList.remove('buttonActive'));
 
 const consumirDatos = async () => {
-    const response = await fetch('charlas.json');
+    const response = await fetch('../charlas.json');
     const data = await response.json();
     charlas = data.charlas;
     renderCharlas();
@@ -52,6 +60,36 @@ const renderCharlas = (e) => {
     });
 }
 
+/* const calcularTiempoPasado = () => {
+    const fechaReferencia = new Date('2023-10-07T00:00:00');
+
+    const fechaActual = new Date();
+    const diferencia = fechaActual - fechaReferencia;
+
+    // Convertir la diferencia de milisegundos a meses, días, horas, minutos y segundos
+    let segundos = Math.floor(diferencia / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    // Calcular los segundos restantes después de calcular los meses, días, horas y minutos
+    const segundosRestantes = segundos % 60;
+
+    dias = Math.floor(dias % 30.436875) // Resto de días
+    horas = horas % 24
+    minutos = minutos % 60
+    segundos = segundosRestantes
+
+    $dias.textContent = dias;
+    $horas.textContent = horas;
+    $minutos.textContent = minutos;
+    $segundos.textContent = segundos;
+}
+
+const iniciarContador = () => {
+    setTimeout
+}
+ */
 
 
 
